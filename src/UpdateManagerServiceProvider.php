95f814071e59203a142e2164b6cf1a7a1921b308
Jalal Al-Mussawi <xidanko07@gmail.com>
2023-03-12 12:42:43 +0300

    publish update migration

--- src/UpdateManagerServiceProvider.php
+++ src/UpdateManagerServiceProvider.php
@@ -13,7 +13,7 @@ class UpdateManagerServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->publishes([__DIR__ . '/../config/update-manager.php' => config_path('update-manager.php')], 'update-manager-config');
+        $this->publishes([__DIR__ . '/../database/migrations/2022_02_21_210710_create_updates_table.php' => database_path('migrations/2022_02_21_210710_create_updates_table.php')], 'update-manager-migration');
     }
 }
